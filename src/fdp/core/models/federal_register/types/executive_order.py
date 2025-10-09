2b844a421c041828d3bfed4239a2d6e5cf9eb191
Enrique <138890026+Cascabel76@users.noreply.github.com>
2025-10-08 21:50:59 -0400

    Wrapped up the executive order data class.

--- src/fdp/core/models/federal_register/types/executive_order.py
+++ src/fdp/core/models/federal_register/types/executive_order.py
@@ -1,8 +1,10 @@
 from presidential_document import PresidentialDocument
 from dataclasses import dataclass
-
+from typing import Optional, List
 
 @dataclass
 class ExecutiveOrder(PresidentialDocument): #this is a child class of the agnostic 'document' class
-    eo_notes: str 
-    eo_number: int
+    eo_notes: Optional[str] 
+    eo_number: Optional[str]
+    amends_eos: List[str]
+    revokes_eops: List[str]
--- src/fdp/core/models/federal_register/types/presidential_document.py
+++ src/fdp/core/models/federal_register/types/presidential_document.py
@@ -6,5 +6,10 @@ from typing import Optional
 @dataclass
 class PresidentialDocument(Document):
     presidental_doc_num: int
+    prcolamation_number: int
     president: str
     signing_date: Optional[date]
+
+# UNSURE OF THESE
+    subtype: str
+    type: str
